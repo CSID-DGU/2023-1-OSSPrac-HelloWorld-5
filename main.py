aa404ad33de1af06cf8abb80827ceec0db930576
Uiyeong Kim <uykim07@dongguk.edu>
2023-05-04 17:45:14 +0900

    김의영, main.py 및 result.html에 표 생성

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from flask import Flask, redirect,render_template,request
 
 app=Flask(__name__)
 
-@app.route('/',methods=['POST','GET'])
-
+@app.route('/')
 def main():
     return render_template('main.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        studentnumber = request.form.get('StudentNumber')
+        major = request.form.get('Major')
+        email_id = request.form.get('email_id')
+        email_addr = request.form.get('email_addr')
+        gender = request.form.get('gender')
+        languages = request.form.getlist('languages')
+
+        return render_template('result.html',name=name,studentnumber=studentnumber,major=major,email_id=email_id,email_addr=email_addr,gender=gender,languages=languages)
 
 if __name__=='__main__':
     app.run(debug=True)
